1c7015f50344df632e5d4f7bb385a411eb0cc9cb
FernandoBeltran1 <fernando333hummelz@gmail.com>
2026-05-09 01:27:14 -0600

    HOLA
    ASS

---
+++ Proyecto_Final_CGeIHC-main/ProyectoFinal/Camera.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <glew.h>
+
+#include <glm.hpp>
+#include <gtc\matrix_transform.hpp>
+
+#include <glfw3.h>
+
+class Camera
+{
+public:
+	Camera();
+	Camera(glm::vec3 startPosition, glm::vec3 startUp, GLfloat startYaw, GLfloat startPitch, GLfloat startMoveSpeed, GLfloat startTurnSpeed);
+
+	void keyControl(bool* keys, GLfloat deltaTime);
+	void mouseControl(GLfloat xChange, GLfloat yChange);
+
+	// ------------------ Cambio de camaras -----------------------
+	void controlarCamara(bool* keys, GLfloat deltaTime, GLfloat xChange, GLfloat yChange, int camaraActual, int vistaEstaticaActual);
+	
+	// ------------------ Cambio de camaras -----------------------
+
+	glm::vec3 getCameraPosition();
+	glm::vec3 getCameraDirection();
+	glm::mat4 calculateViewMatrix();
+
+	~Camera();
+
+private:
+	glm::vec3 position;
+	glm::vec3 front;
+	glm::vec3 up;
+	glm::vec3 right;
+	glm::vec3 worldUp;
+
+	GLfloat yaw;
+	GLfloat pitch;
+
+	GLfloat moveSpeed;
+	GLfloat turnSpeed;
+
+	void update();
+
+	// ------------------ Cambio de camaras -----------------------
+	struct EstadoCamara {
+		glm::vec3 position;
+		GLfloat yaw;
+		GLfloat pitch;
+	};
+
+	EstadoCamara estadosCamara[5];
+
+	int camaraAnterior;
+
+	void guardarEstado(int camara);
+	void restaurarEstado(int camara);
+	void keyControlXZ(bool* keys, GLfloat deltaTime, GLfloat alturaFija);
+	void camaraEstatica(int vistaEstaticaActual);
+	// ------------------ Cambio de camaras -----------------------
+
+};
+
